16d89ea117270bbb02e057fdda4683f86cbaf70b
krishna <dhekkrishna1@gmail.com>
2025-08-17 18:49:27 +0530

    Using array to add elements in the same level for nested elements

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
 
 //nested elements in react
 
-const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "Hello I'm a heading tag in a nested set"), React.createElement('h2',{}, "Hey another tag" )]));
+const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "Hello I'm a heading tag in a nested set"), React.createElement('h2',{}, "Hey another tag" ), React.createElement("h3",{},"Again a heading tag")]));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
