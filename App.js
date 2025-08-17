a1372c8f5c6a76ddc4b9d8b1cadc6089f79e59a2
krishna <dhekkrishna1@gmail.com>
2025-08-17 18:36:19 +0530

    Add separate js and css files

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h1', {id:"heading"}, 'Welcome to React Journey'); //createElement creates a react element has 3 args
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
