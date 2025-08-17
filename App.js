222529e0ed37a65c2c49f3dd894f6b99e9fc3f6a
krishna <dhekkrishna1@gmail.com>
2025-08-17 18:48:05 +0530

    Creating Nested Elements in React

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading = React.createElement('h1', {id:"heading"}, 'Welcome to React Journey'); //createElement creates a react element has 3 args
+// const heading = React.createElement('h1', {id:"heading"}, 'Welcome to React Journey'); //createElement creates a react element has 3 args
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+
+//nested elements in react
+
+const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "Hello I'm a heading tag in a nested set"), React.createElement('h2',{}, "Hey another tag" )]));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
