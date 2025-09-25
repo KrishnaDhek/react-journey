3509eba7df0b9d23ff0b8cad1ff88c7e44eba592
krishna <dhekkrishna1@gmail.com>
2025-09-25 20:26:16 +0530

    Expense Tracker using React

--- Projects/expense-tracker-react/src/App.js
+++ Projects/expense-tracker-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/header';
+import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
