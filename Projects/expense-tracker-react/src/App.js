ee8e2de3674ad6c34d82347238906278a30739d2
krishna <dhekkrishna1@gmail.com>
2025-09-25 16:34:10 +0530

    Add transation logic

--- Projects/expense-tracker-react/src/App.js
+++ Projects/expense-tracker-react/src/App.js
@@ -5,11 +5,13 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
 
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -18,7 +20,7 @@ function App() {
         <AddTransaction />
       </div>
 
-    </div>
+    </GlobalProvider>
   );
 }
 
--- Projects/expense-tracker-react/src/components/Balance.js
+++ Projects/expense-tracker-react/src/components/Balance.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+    const { transactions } = React.useContext(GlobalContext);
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    
   return (
       <>
           <h2>Your Balance</h2>
-          <h1>$0.00</h1>
+          <h1>${total }</h1>
       </>
   )
 }
